809d96272f4679b3784b603e74d8f4eca8b511d5
dukelester <dukelester4@gmail.com>
2023-04-14 22:19:28 +0300

    The basics of the routes and urls in  flask

--- simple_flask/app.py
+++ simple_flask/app.py
@@ -5,4 +5,72 @@ print('Welcome to the simple flask application...')
 
 @app.get('/')
 def index_page():
-    return ' Welcome to the index page !'
+    return '<h3>Welcome to the index page ! </h3>'
+
+@app.get('/hello')
+def get_hello_world():
+    return '<h2>Hello this is just an introduction'
+
+@app.route('/users', methods=['GET', 'POST'])
+def get_users():
+    return [{
+        'name': 'Duke lester',
+        'phone': '0799445562',
+        'job': 'Software Engineer',
+        'country': 'Kenya',
+        'address': {
+            'zip': 2333,
+            'postal code': 62000,
+            'town name': 'Kiambu, Juja',
+        },
+    },
+            {
+        'name': 'Kennedy Mongwa',
+        'phone': '0799407662',
+        'job': 'Software Engineer',
+        'country': 'Kenya',
+        'address': {
+            'zip': 3400,
+            'postal code': 92000,
+            'town name': 'Mombasa',
+        },
+    },
+        {
+        'name': 'master man',
+        'phone': '0799405562',
+        'job': 'CRM Engineer',
+        'country': 'Uganda',
+        'address': {
+            'zip': 2033,
+            'postal code': 69000,
+            'town name': 'Kampala',
+        },
+    },          
+        {
+        'name': 'Jason Kimani',
+        'phone': '079945562',
+        'job': 'Software Developer',
+        'country': 'Kenya',
+        'address': {
+            'zip': 2303,
+            'postal code': 60000,
+            'town name': 'Nairobi',
+        },
+    },
+    ]
+
+@app.route('/users/<int:user_id>')
+def get_user_details(user_id: int):
+    print('user id', user_id)
+    return {
+        'userId': user_id,
+        'name': 'Duke lester',
+        'phone': '0799445562',
+        'job': 'Software Engineer',
+        'country': 'Kenya',
+        'address': {
+            'zip': 2333,
+            'postal code': 62000,
+            'town name': 'Kiambu, Juja',
+        },
+    }
